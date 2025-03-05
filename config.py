87441bf65eb456bec99d31377ee7777c7842c64c
IDGS-805-21000375 <21000375@alumnos.utleon.edu.mx>
2025-03-04 18:55:18 -0600

    Agregar 04/03/25

--- config.py
+++ config.py
@@ -2,12 +2,12 @@ import os
 from sqlalchemy import create_engine
 import urllib
 
-class config(object):
-    SECRET_KEY='Clave nueva'
-    SESSION_COOKIE_SECURE=False
-   
-class DevelopmentConfig(config):
-    DEBUF=True
-    AQLALCHEMY_DATABASE_URL='mysql+pymysql://admin:root127.0.0.1/bdidgs805'
-    SQLALCHEMY_TRACK_MODIFICATIONS=False
-    
+class Config(object):
+    SECRET_KEY = 'Clave nueva'
+    SESSION_COOKIE_SECURE = False
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:admin@127.0.0.1/bdidgs805'  # Tu URI de base de datos
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
--- models.py
+++ models.py
@@ -1,11 +1,13 @@
-from flask_sqlalchemy import SQAlchemy
+from flask_sqlalchemy import SQLAlchemy
+
 import datetime
 
-db-SQAlchemy()
+db = SQLAlchemy()
+
 class Alumno(db.Model):
     __tablename__='alumnos'
     id=db.Column(db.Integer,primary_key=True)
-    nombre=db.Colum(db.String(50))
-    apaterno=db.Colum(db.String(50))
-    email=db.Colum(db.String(50))
+    nombre=db.Column(db.String(50))
+    apaterno=db.Column(db.String(50))
+    email=db.Column(db.String(50))
     created_date=db.Column(db.DateTime,default=datetime.datetime.now)
